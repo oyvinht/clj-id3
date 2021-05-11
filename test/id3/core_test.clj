5c0ca8f3ac5021b84e351deea7d9fe2a4586ec42
Øyvin Halfdan Thuv <oyvin.h.thuv@uit.no>
2021-05-11 21:48:50 +0200

    Tidying up tests. Adding test for classify.

--- test/id3/core_test.clj
+++ test/id3/core_test.clj
@@ -4,29 +4,47 @@
             [criterium.core :as criterium]))
 
 (def examples
-  [{:outlook "sunny"    :temp "hot"  :humidity "high"   :wind "weak"   :play-tennis "no"}
-   {:outlook "sunny"    :temp "hot"  :humidity "high"   :wind "strong" :play-tennis "no"}
-   {:outlook "overcast" :temp "hot"  :humidity "high"   :wind "weak"   :play-tennis "yes"}
-   {:outlook "rain"     :temp "mild" :humidity "high"   :wind "weak"   :play-tennis "yes"}
-   {:outlook "rain"     :temp "cool" :humidity "normal" :wind "weak"   :play-tennis "yes"}
-   {:outlook "rain"     :temp "cool" :humidity "normal" :wind "strong" :play-tennis "no"}
-   {:outlook "overcast" :temp "cool" :humidity "normal" :wind "strong" :play-tennis "yes"}
-   {:outlook "sunny"    :temp "mild" :humidity "high"   :wind "weak"   :play-tennis "no"}
-   {:outlook "sunny"    :temp "cool" :humidity "normal" :wind "weak"   :play-tennis "yes"}
-   {:outlook "rain"     :temp "mild" :humidity "normal" :wind "weak"   :play-tennis "yes"}
-   {:outlook "sunny"    :temp "mild" :humidity "normal" :wind "strong" :play-tennis "yes"}
-   {:outlook "overcast" :temp "mild" :humidity "high"   :wind "strong" :play-tennis "yes"}
-   {:outlook "overcast" :temp "hot"  :humidity "normal" :wind "weak"   :play-tennis "yes"}
-   {:outlook "rain"     :temp "mild" :humidity "high"   :wind "strong" :play-tennis "no"}])
+  [{:outlook "sunny"    :temp "hot"  :humidity "high"   :wind "weak"
+    :play-tennis "no"}
+   {:outlook "sunny"    :temp "hot"  :humidity "high"   :wind "strong"
+    :play-tennis "no"}
+   {:outlook "overcast" :temp "hot"  :humidity "high"   :wind "weak"
+    :play-tennis "yes"}
+   {:outlook "rain"     :temp "mild" :humidity "high"   :wind "weak"
+    :play-tennis "yes"}
+   {:outlook "rain"     :temp "cool" :humidity "normal" :wind "weak"
+    :play-tennis "yes"}
+   {:outlook "rain"     :temp "cool" :humidity "normal" :wind "strong"
+    :play-tennis "no"}
+   {:outlook "overcast" :temp "cool" :humidity "normal" :wind "strong"
+    :play-tennis "yes"}
+   {:outlook "sunny"    :temp "mild" :humidity "high"   :wind "weak"
+    :play-tennis "no"}
+   {:outlook "sunny"    :temp "cool" :humidity "normal" :wind "weak"
+    :play-tennis "yes"}
+   {:outlook "rain"     :temp "mild" :humidity "normal" :wind "weak"
+    :play-tennis "yes"}
+   {:outlook "sunny"    :temp "mild" :humidity "normal" :wind "strong"
+    :play-tennis "yes"}
+   {:outlook "overcast" :temp "mild" :humidity "high"   :wind "strong"
+    :play-tennis "yes"}
+   {:outlook "overcast" :temp "hot"  :humidity "normal" :wind "weak"
+    :play-tennis "yes"}
+   {:outlook "rain"     :temp "mild" :humidity "high"   :wind "strong"
+    :play-tennis "no"}])
 
 (deftest test-subset
   (testing "See if subset works as intended."
-    (is (= [{:outlook "overcast" :temp "hot"  :humidity "normal" :wind "weak"   :play-tennis "yes"}
-            {:outlook "overcast" :temp "mild" :humidity "high"   :wind "strong" :play-tennis "yes"}
-            {:outlook "overcast" :temp "cool" :humidity "normal" :wind "strong" :play-tennis "yes"}
-            {:outlook "overcast" :temp "hot"  :humidity "high"   :wind "weak"   :play-tennis "yes"}]
+    (is (= [{:outlook "overcast" :temp "hot"  :humidity "normal" :wind "weak"
+             :play-tennis "yes"}
+            {:outlook "overcast" :temp "mild" :humidity "high"   :wind "strong"
+             :play-tennis "yes"}
+            {:outlook "overcast" :temp "cool" :humidity "normal" :wind "strong"
+             :play-tennis "yes"}
+            {:outlook "overcast" :temp "hot"  :humidity "high"   :wind "weak"
+             :play-tennis "yes"}]
            (#'id3.core/subset examples :outlook "overcast")))))
-  
+
 (deftest test-attr-val-counts
   (testing "Test count number of attributes with a specific value."
     (is (= {"sunny" 5 "overcast" 4 "rain" 5}
@@ -45,7 +63,8 @@
 (deftest test-most-info-gaining-attr []
   (testing "Testing if most-info-gaining-attribute function works."
     (is (= :outlook
-           (#'id3.core/most-info-gaining-attr [:humidity :outlook :temp :wind] examples :play-tennis)))))
+           (#'id3.core/most-info-gaining-attr
+            [:humidity :outlook :temp :wind] examples :play-tennis)))))
 
 (deftest test-most-common-attr-val []
   (testing "Testing if most-common-attribute-value function works."
@@ -58,3 +77,11 @@
       (is (= "no" (get-in result [:outlook "rain" :wind "strong"])))
       (is (= "yes" (get-in result [:outlook "overcast"])))
       (is (= "no" (get-in result [:outlook "sunny" :humidity "high"]))))))
+
+(deftest test-classify []
+  (testing "Testing if classify classifies correctly."
+    (let [dtree (id3 examples :play-tennis "yes" "no")]
+      (is
+       (= "no"
+          (classify
+           {:humidity "high" :outlook "rain" :temp "mild" :wind "weak"}))))))
